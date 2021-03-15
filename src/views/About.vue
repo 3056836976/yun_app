8d640b3409cbeb647ecad1ad382e51ecc6098619
钟声亮 <3056836976@qq.com>
2021-03-15 19:28:12 +0800

    init

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <el-button type="danger">危险按钮</el-button>
+    <el-button type="danger" plain>危险按钮</el-button>
+    <el-button type="danger" round>危险按钮</el-button>
+  </div>
+
+</template>
